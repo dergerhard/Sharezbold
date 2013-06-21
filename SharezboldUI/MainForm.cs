9e0f4b431a34484f282835db0e4b3987d57fbdb1
tomhuizi <tomhuizi@hotmail.de>
2013-06-21 18:43:52 +0200

    added project SharezboldUI as real UI-project

---
+++ SharezboldUI/MainForm.cs
@@ -0,0 +1,30 @@
+﻿namespace SharezboldUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Exit the application
+        /// </summary>
+        /// <param name="sender">sender of the action</param>
+        /// <param name="e">the event which was executed</param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
