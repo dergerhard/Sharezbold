a833774c4d9e462e242cece584222f77b9992cef
tomhuizi <tomhuizi@hotmail.de>
2013-06-21 20:39:27 +0200

    added MigrationUtils library. it is necessary for different subprojects of our Sharezbold

---
+++ SharezboldUI/MigrationUtils/SharepointInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace Sharezbold.Util
+{
+    using System.Net;
+
+    /// <summary>
+    /// Data for the sharepoint-information.
+    /// </summary>
+    public class SharepointInformation
+    {
+        public string Address{ get; set; }
+        public NetworkCredential Credentials { get; set; }
+    }
+}
