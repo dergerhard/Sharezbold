aa40979c41024feab413bfe63f8bd1244c7998a4
tomhuizi <tomhuizi@hotmail.de>
2013-07-08 08:16:42 +0200

    added docs

--- TypeMigration/IElementsMigrator.cs
+++ TypeMigration/IElementsMigrator.cs
@@ -1,18 +1,51 @@
-﻿namespace Sharezbold.ElementsMigration
+﻿//-----------------------------------------------------------------------
+// <copyright file="IElementsMigrator.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Thomas Holzgethan (35224@fhwn.ac.at)</author>
+//-----------------------------------------------------------------------
+namespace Sharezbold.ElementsMigration
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
+    /// <summary>
+    /// Interface for migrate between different SharePoint-Versions.
+    /// To use the concret SharePoint version use the class <see cref="Sharepoint2010And2013Migrator"/> for SharePoint 2010 and 2013.
+    /// </summary>
     public interface IElementsMigrator
     {
+        /// <summary>
+        /// Migrates the ContentTypes.
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigrateContentTypes();
+
+        /// <summary>
+        /// Migrates the SiteColumns.
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigrateSiteColumns();
+
+        /// <summary>
+        /// Migrates the User.
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigrateUser();
+
+        /// <summary>
+        /// Migrates the Group.
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigrateGroup();
+
+        /// <summary>
+        /// Migrates the PermissionLevels (Role).
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigratePermissionlevels();
+
+        /// <summary>
+        /// Migrates the Workflow.
+        /// </summary>
+        /// <exception cref="ElementsMigrationException">If the migration fails.</exception>
         void MigrateWorkflow();
     }
 }
