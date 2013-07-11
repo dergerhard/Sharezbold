736740cd17a8020ab9f5f46e1edd16f75e72a7e5
tomhuizi <tomhuizi@hotmail.de>
2013-07-11 19:48:03 +0200

    added extension for roles

---
+++ TypeMigration/Extensions/RoleExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Sharezbold.ElementsMigration.Extension
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.SharePoint.Client;
+
+    internal static class RoleExtensions
+    {
+
+        /// <summary>
+        /// Returns all names of given RoleDefinitions as HashSet.
+        /// </summary>
+        /// <param name="roleDefinitions">RoleDefinitions to read th names</param>
+        /// <returns>names of RoleDefinitions</returns>
+        internal static HashSet<string> ReadNames(this RoleDefinitionCollection roleDefinitions)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            foreach (var roleDefinition in roleDefinitions)
+            {
+                names.Add(roleDefinition.Name);
+            }
+
+            return names;
+        }
+    }
+}
