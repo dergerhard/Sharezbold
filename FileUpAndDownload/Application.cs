739d8753804fc66fa245cb3776e19491705b034b
tomhuizi <tomhuizi@hotmail.de>
2013-10-15 22:56:23 +0200

    created main-class to test file-upload

---
+++ FileUpAndDownload/Application.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using Microsoft.SharePoint.Client;
+
+namespace Sharezbold.FileMigration
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start File-Migration");
+
+            ClientContext source = new ClientContext("http://10.10.102.36");
+            ClientContext target = new ClientContext("http://10.10.102.38");
+            NetworkCredential credentials = new NetworkCredential("Administrator", "P@ssw0rd", "cssdev");
+
+            source.Credentials = credentials;
+
+            SharePoint2010And2013FileMigrator migrator = new SharePoint2010And2013FileMigrator(source, target);
+
+            try
+            {
+                migrator.DownloadDocument("images", "sharepoint2010.png");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.WriteLine("Finished File-Migartion");
+            Console.ReadKey();
+        }
+    }
+}
