999789e33f5a5c716179897c4a89c4fd60f14df1
Thomas <thomas.holzgethan@outlook.com>
2013-10-18 00:01:14 +0200

    added new implementation of file-migration

--- FileUpAndDownload/Application.cs
+++ FileUpAndDownload/Application.cs
@@ -16,6 +16,35 @@ namespace Sharezbold.FileMigration
         {
             Console.WriteLine("Start File-Migration");
             ClientContext source = new ClientContext("http://10.10.102.36");
+            ClientContext target = new ClientContext("http://10.10.102.37");
+            NetworkCredential credentials = new NetworkCredential("Administrator", "P@ssw0rd", "CSSDEV");
+
+            source.Credentials = credentials;
+            target.Credentials = credentials;
+
+            Web web = source.Web;
+            ListCollection lists = web.Lists;
+
+            source.Load(web);
+            source.Load(lists);
+            source.ExecuteQuery();
+            target.Load(target.Web);
+            target.ExecuteQuery();
+
+            SharePoint2010And2013Migrator migrator = new SharePoint2010And2013Migrator(source, target);
+
+            List<string> guids = new List<string>();
+            
+            foreach (var list in lists)
+            {
+                guids.Add(list.Id.ToString());
+            }
+
+            migrator.MigrateFiles(guids);
+
+            /*
+            Console.WriteLine("Start File-Migration");
+            ClientContext source = new ClientContext("http://10.10.102.36");
             ClientContext target = new ClientContext("http://10.10.102.36");
             NetworkCredential credentials = new NetworkCredential("Administrator", "P@ssw0rd", "CSSDEV");
 
@@ -28,21 +57,13 @@ namespace Sharezbold.FileMigration
             string documentName = "sharepoint2010.png";
             string documentType = "Image";
             string documentNewName = "sharepoint2010_new.png";
-            Stream inputStream = migrator.DownloadDocument(documentListName, documentName, documentType);
+            Stream inputStream = migrator.DownloadDocument(documentListName, documentName);
             migrator.UploadDocument(documentListName, migrator.RelativeUrl + "new.png", StreamToByte(inputStream));
 
             inputStream.Close();
+             * */
             Console.WriteLine("Finished File-Migartion");
             Console.ReadKey();
-        }
-
-        private static byte[] StreamToByte(Stream stream)
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                stream.CopyTo(memoryStream);
-                return memoryStream.ToArray();
-            }
         }
     }
 }
