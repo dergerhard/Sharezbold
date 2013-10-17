abf3d03ca6316f87af9573925c73ab4feef80ad8
Thomas <thomas.holzgethan@outlook.com>
2013-10-17 07:56:35 +0200

    implemented download of file via ClientObjectModel

--- FileUpAndDownload/Application.cs
+++ FileUpAndDownload/Application.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
 
 using Microsoft.SharePoint.Client;
 
@@ -20,17 +21,37 @@ namespace Sharezbold.FileMigration
 
             source.Credentials = credentials;
             target.Credentials = credentials;
-            
-           // SharePoint2010And2013FileMigrator migrator = new SharePoint2010And2013FileMigrator(source, target);
 
+            SharePoint2010And2013FileMigrator migrator = new SharePoint2010And2013FileMigrator(source, target);
+
+            string documentListName = "Documents";
+            string documentName = "sharepoint2010.png";
+            string documentType = "Image";
+            Stream inputStream = migrator.DownloadDocument(documentListName, documentName, documentType);
+
+            string outputFile = @"C:\temp\sharepoint2010.png";
+            using (Stream outputStream = System.IO.File.OpenWrite(outputFile))
+            {
+                byte[] buffer = new byte[8 * 1024];
+                int len;
+                while ((len = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outputStream.Write(buffer, 0, len);
+                    outputStream.Flush();
+                }
+                outputStream.Close();
+            }
+
+            inputStream.Close();
+            /*
             string destinationFile = "http://10.10.102.36/images/sharepoint_new_2010.png";
             string sourceFile = "http://10.10.102.36/images/sharepoint2010.png";
 
             SharePointFileMigrator migrator = new SharePointFileMigrator(source, target);
             migrator.CopyDocuments(sourceFile, destinationFile);
             //migrator.Migrate(sourceFile, destinationFile);
-
-        //http://blogs.msdn.com/b/sridhara/archive/2010/03/12/uploading-files-using-client-object-model-in-sharepoint-2010.aspx
+            */
+            //http://blogs.msdn.com/b/sridhara/archive/2010/03/12/uploading-files-using-client-object-model-in-sharepoint-2010.aspx
             // http://www.codeproject.com/Articles/103503/How-to-upload-download-a-document-in-SharePoint-20
             /*
             Console.WriteLine("Start File-Migration");
--- FileUpAndDownload/SharePoint2010And2013FileMigrator.cs
+++ FileUpAndDownload/SharePoint2010And2013FileMigrator.cs
@@ -49,7 +49,7 @@ namespace Sharezbold.FileMigration
             Stream fileStream = null;
             try
             {
-                fileStream = this.DownloadDocument(documentListName, documentName);
+                //fileStream = this.DownloadDocument(documentListName, documentName);
             }
             catch (FileNotFoundException e)
             {
@@ -82,9 +82,10 @@ namespace Sharezbold.FileMigration
         /// <param name="documentName">document name to download</param>
         /// <returns>downloaded file as stream</returns>
         /// <exception cref="FileNotFoundException">if the file was not found</exception>
-        internal Stream DownloadDocument(string documentListName, string documentName)
+        internal Stream DownloadDocument(string documentListName, string documentName, string documentType)
         {
-            ListItem item = GetDocumentFromSharePoint(documentListName, documentName);
+            string fileReference = "FileLeafRef";
+            ListItem item = GetDocumentFromSharePoint(documentListName, fileReference, documentName, documentType);
             if (item != null)
             {
                 FileInformation fileInformation = Microsoft.SharePoint.Client.File.OpenBinaryDirect(sourceClientContext,
@@ -162,10 +163,10 @@ namespace Sharezbold.FileMigration
         /// <param name="documentListName">name of the documentlist</param>
         /// <param name="documentName">name of the document</param>
         /// <returns>document from SharePoint as ListItem</returns>
-        private ListItem GetDocumentFromSharePoint(string documentListName, string documentName)
+        private ListItem GetDocumentFromSharePoint(string documentListName, string fileReference, string documentName, string documentType)
         {
-            ListItemCollection listItems = GetListItemCollectionFromSharePoint(documentListName, "FileLeafRef",
-                documentName, "Text", 1);
+            ListItemCollection listItems = GetListItemCollectionFromSharePoint(documentListName, fileReference,
+                documentName, documentType, 1);
 
             return (listItems != null && listItems.Count == 1) ? listItems[0] : null;
         }
