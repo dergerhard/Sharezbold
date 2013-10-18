7bd0b69714dc3a3627939651eb1c057fb4b5da06
Thomas <thomas.holzgethan@outlook.com>
2013-10-18 10:34:42 +0200

    implemented check of new server version

---
+++ FileUpAndDownload/SharePointNotSupportedException.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Sharezbold.FileMigration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SharePointNotSupportedException : Exception
+    {
+        public SharePointNotSupportedException(string message) : base(message)
+        {
+        }
+    }
+}
