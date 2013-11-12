c9c9072f88b812e958289088573854078f6683f8
Thomas <thomas.holzgethan@outlook.com>
2013-11-12 13:25:57 +0100

    added sample-code of file-migration

--- FileUpAndDownload/Application.cs
+++ FileUpAndDownload/Application.cs
@@ -4,7 +4,7 @@
 // </copyright>
 // <author>Thomas Holzgethan (35224@fhwn.ac.at)</author>
 //-----------------------------------------------------------------------
-/*
+
 using Microsoft.SharePoint.Client;
 
 namespace Sharezbold.FileMigration
@@ -22,12 +22,13 @@ namespace Sharezbold.FileMigration
         public static void Main(string[] args)
         {
             Console.WriteLine("Start File-Migration");
-            ClientContext source = new ClientContext("http://10.10.102.36");
-            ClientContext target = new ClientContext("http://10.10.102.48");
-            NetworkCredential credentials = new NetworkCredential("Administrator", "P@ssw0rd", "CSSDEV");
+            ClientContext source = new ClientContext("http://sps2010003");
+            ClientContext target = new ClientContext("http://sps2013003");
+            NetworkCredential credentialsSource = new NetworkCredential("Administrator", "P@ssw0rd", "CSSSPS2013003");
+            NetworkCredential credentialsTargat = new NetworkCredential("Administrator", "P@ssw0rd", "CSSSPS2010003");
 
-            source.Credentials = credentials;
-            target.Credentials = credentials;
+            source.Credentials = credentialsSource;
+            target.Credentials = credentialsTargat;
 
             Web web = source.Web;
             ListCollection lists = web.Lists;
@@ -40,6 +41,12 @@ namespace Sharezbold.FileMigration
 
             Console.WriteLine("Sharepoint 2010 version = {0}", target.ServerVersion.Major);
             Console.WriteLine("Sharepoint 2013 version = {0}", source.ServerVersion.Major);
+
+            SharePoint2010And2013Migrator migrator = new SharePoint2010And2013Migrator(source, target);
+            migrator.MigrateFilesOfWeb(source.Web, target.Web);
+
+            Console.WriteLine("finished program");
+            Console.ReadKey();
 
             /*
             SharePoint2010And2013Migrator migrator = new SharePoint2010And2013Migrator(source, target);
@@ -76,7 +83,7 @@ namespace Sharezbold.FileMigration
              
             Console.WriteLine("Finished File-Migartion");
             Console.ReadKey();
+             * */
         }
     }
 }
-*/
