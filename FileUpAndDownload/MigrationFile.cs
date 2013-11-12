ef2a234d04df18b9cc01621fb2b3869c5cc8fb5b
Thomas <thomas.holzgethan@outlook.com>
2013-11-12 12:55:31 +0100

    Added object for file to migrate

---
+++ FileUpAndDownload/MigrationFile.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Sharezbold.FileMigration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.SharePoint.Client;
+
+    internal class MigrationFile
+    {
+        internal File File { get; set; }
+        internal System.IO.Stream DownloadedStream { get; set; }
+    }
+}
