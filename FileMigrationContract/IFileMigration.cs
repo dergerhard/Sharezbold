a71938690eb7b2f1d72b324fe4c1a8b6f5ac6589
Thomas <thomas.holzgethan@outlook.com>
2013-11-12 19:57:29 +0100

    added contract project for webservice to use full functionality of FileMigration

---
+++ FileMigrationContract/IFileMigration.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Sharezbold.FileMigration.Contract
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface IFileMigration
+    {
+        [OperationContract]
+        int GetMaxFileSize();
+
+        [OperationContract]
+        IDictionary<string, int> GetMaxFileSizePerExtension();
+    }
+}
