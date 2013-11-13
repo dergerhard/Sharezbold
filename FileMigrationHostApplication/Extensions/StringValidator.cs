030759dbbee5c78f7b76ca3a3f1f093d28e3b2a9
Thomas <thomas.holzgethan@outlook.com>
2013-11-13 21:03:30 +0100

    added a string-validator for user inputs

---
+++ FileMigrationHostApplication/Extensions/StringValidator.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Sharezbold.FileMigration.Host.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class StringValidator
+    {
+        internal static void IsNullOrEmpty(this string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                throw new ValidationException("string must not be null or empty");
+            }
+        }
+
+        internal static void IsNumberic(this string str)
+        {
+            int number;
+            if (!int.TryParse(str, out number))
+            {
+                throw new ValidationException("string is not a number");
+            }
+        }
+    }
+}
---
+++ FileMigrationHostApplication/ValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharezbold.FileMigration.Host
+{
+    internal class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message)
+        {
+        }
+    }
+}
