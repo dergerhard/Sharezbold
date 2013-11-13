173b68c521550cbc5edefd3a31fa27f0c0c0d160
Thomas <thomas.holzgethan@outlook.com>
2013-11-13 06:56:33 +0100

    * renamed Form1 to Main Window * implemented UI

---
+++ FileMigrationHostApplication/MainWindow.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Sharezbold.FileMigration.Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxServiceAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxProtocol = new System.Windows.Forms.ComboBox();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.buttonStartStopService = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShapeStatus = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxServiceAddress
+            // 
+            this.textBoxServiceAddress.Location = new System.Drawing.Point(167, 17);
+            this.textBoxServiceAddress.Name = "textBoxServiceAddress";
+            this.textBoxServiceAddress.Size = new System.Drawing.Size(156, 20);
+            this.textBoxServiceAddress.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(99, 48);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(94, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Service-URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Service-Port";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBoxProtocol);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxPort);
+            this.panel1.Controls.Add(this.textBoxServiceAddress);
+            this.panel1.Location = new System.Drawing.Point(27, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(335, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBoxProtocol
+            // 
+            this.comboBoxProtocol.FormattingEnabled = true;
+            this.comboBoxProtocol.Items.AddRange(new object[] {
+            "http://"});
+            this.comboBoxProtocol.Location = new System.Drawing.Point(99, 16);
+            this.comboBoxProtocol.Name = "comboBoxProtocol";
+            this.comboBoxProtocol.Size = new System.Drawing.Size(61, 21);
+            this.comboBoxProtocol.TabIndex = 4;
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(28, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(349, 25);
+            this.labelHeader.TabIndex = 2;
+            this.labelHeader.Text = "FileMigration - Service Manager";
+            // 
+            // buttonStartStopService
+            // 
+            this.buttonStartStopService.Location = new System.Drawing.Point(385, 89);
+            this.buttonStartStopService.Name = "buttonStartStopService";
+            this.buttonStartStopService.Size = new System.Drawing.Size(108, 23);
+            this.buttonStartStopService.TabIndex = 3;
+            this.buttonStartStopService.Text = "Start Service";
+            this.buttonStartStopService.UseVisualStyleBackColor = true;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShapeStatus});
+            this.shapeContainer1.Size = new System.Drawing.Size(505, 197);
+            this.shapeContainer1.TabIndex = 4;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShapeStatus
+            // 
+            this.rectangleShapeStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rectangleShapeStatus.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShapeStatus.Location = new System.Drawing.Point(415, 10);
+            this.rectangleShapeStatus.Name = "rectangleShapeStatus";
+            this.rectangleShapeStatus.Size = new System.Drawing.Size(75, 58);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 197);
+            this.Controls.Add(this.buttonStartStopService);
+            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "FileMigration - Host Application";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxServiceAddress;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBoxProtocol;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Button buttonStartStopService;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShapeStatus;
+    }
+}
+
