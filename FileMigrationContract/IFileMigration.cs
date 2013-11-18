4911962c785e6d0e749fb55db917e7fb1deb44cb
Thomas <thomas.holzgethan@outlook.com>
2013-11-18 22:16:09 +0100

    added style-cop conformed code

--- FileMigrationContract/IFileMigration.cs
+++ FileMigrationContract/IFileMigration.cs
@@ -1,22 +1,39 @@
-﻿
+﻿//-----------------------------------------------------------------------
+// <copyright file="IFileMigration.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <author>Thomas Holzgethan (35224@fhwn.ac.at)</author>
+//-----------------------------------------------------------------------
+
 namespace Sharezbold.FileMigration.Contract
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.ServiceModel;
 
+    /// <summary>
+    /// Interface of the webservice of FileMigration.
+    /// </summary>
     [ServiceContract]
     public interface IFileMigration
     {
+        /// <summary>
+        /// Gets the maximal file size.
+        /// </summary>
+        /// <returns>max file size</returns>
         [OperationContract]
         int GetMaxFileSize();
 
+        /// <summary>
+        /// Returns the max file size per extension as dictionary.
+        /// </summary>
+        /// <returns>max file size per extension</returns>
         [OperationContract]
         IDictionary<string, int> GetMaxFileSizePerExtension();
 
+        /// <summary>
+        /// Returns the max message size.
+        /// </summary>
+        /// <returns>max message size</returns>
         [OperationContract]
         int GetMaxMessageSize();
     }
